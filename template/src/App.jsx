0878e88f0a85757935983b4040dc4adf37de360d
Thinakaran <thinakaranmanohar@yahoo.com>
2025-07-20 11:56:22 +0530

    Pic2

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Template from './.temp/Template'
+import { Home } from './pages'
 
 const App = () => {
-    return <Template />
+    // return <Template />
+    return <Home />
 }
 
 export default App
---
+++ template/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='font-bold text-4xl flex justify-center items-center h-screen '>Edit Here. Have a Great Day! 🚀</div>
+    )
+}
+
+export default Home
